8bd991f305a14b826784d3c61f4a0cd89dbfe098
creeshrimper_fork <creeshrimper@gmail.com>
2025-01-23 16:24:07 +0800

    1.調整迴圈列表的方式 2.config新增了最新的測試用DOC_ID

--- config/config.js
+++ config/config.js
@@ -3,6 +3,9 @@ export const config = {
     // DOC_ID: '1UQFRQhqYEiKI8T_Ilx4DE9r-oaBV1WsUksBxo1ckHtY',
 
     // 11/21副本
-    DOC_ID: '1MrxbXSu9-LmnawEg8CXWwvqKA1_vVFvG_WfaKzTXybc',
+    // DOC_ID: '1MrxbXSu9-LmnawEg8CXWwvqKA1_vVFvG_WfaKzTXybc',
+
+    // 11/23副本
+    DOC_ID: '1VMduQvqVDUqC92HOLnSKNQQIxI6O5CiP3m6423M2TFI',
     BF_LIST_SHEET_ID: '324201252',
 }
--- scripts/sheetCharBf.js
+++ scripts/sheetCharBf.js
@@ -12,18 +12,22 @@ async function searchBuff(characterName) {
 
         // 角色搜尋
         const characters = []
-        for (const [index, row] of rows.entries()) {
-            // if (row._rawData[0]) characters.push(row._rawData[0])
+        // 迴圈每一個row          (第一列被當成了 _headerValues 所以總數會少一行喔)
+        for (let i = 0; i < rows.length; i++) {
+            const row = rows[i]
+
+            // 如果找到對應腳色
             if (row._rawData[0] && row._rawData[0] === characterName) {
-                // console.log(row, `row${index}`)
-                console.log(row._rawData, `row${index}`)
+                const row2 = rows[i + 1]
+                console.log(row._rawData, `row${i}`)
+                console.log(row2._rawData, `row${i + 1}`)
+
+                // 找到了後面不用再跑
+                break
             }
         }
-        // rows.forEach((row, i) => {
-        //     console.log(row)
-        // })
 
-        // // 角色搜尋
+        // 角色搜尋
         if (characters.includes(characterName)) {
             console.log(`顯示 ${characterName} 角色BUFF`)
         } else {
